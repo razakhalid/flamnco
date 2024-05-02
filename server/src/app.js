4134882c8f40f8e92c94976be9eb11a5b0b44dec
razakhalid <ra97za@gmail.com>
2024-05-02 02:58:51 -0500

    updated dist path

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(cors({
   origin: "http://localhost:4200"
 }));
-app.use(express.static(path.join(__dirname, "..", "..", "client", "dist")));
+app.use(express.static(path.join(__dirname, "..", "..", "client", "dist", "client", "browser")));
 
 app.get('/api', (req, res) => {
   res.status(200).json({
