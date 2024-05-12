1d2aa42df893f64dc3221deba69474c6041860f2
ramil akhmedov <ramil.akhmedov96@gmail.com>
2024-05-12 15:31:35 -0500

    created contact page and I add contact route to the app.routes.ts

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
-import {ShopPageComponent} from "./shop-page/shop-page.component";
+import { ShopPageComponent } from "./shop-page/shop-page.component";
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
-  { path: '', component: ShopPageComponent }
+  { path: '', component: ShopPageComponent },
+  { path: 'contact', component: ContactComponent} //Did this
+
 ];
+
+
