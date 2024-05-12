72b046e3475bf8d090c68cfafea1285f646c7dd3
razakhalid <ra97za@gmail.com>
2024-05-12 18:34:50 -0500

    ran build

--- client/src/app/components/nav/nav.component.ts
+++ client/src/app/components/nav/nav.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
