1009326f02273961a24ae7dc17e7a685295cbe4c
plubgoard <mlitak8@gmail.com>
2024-05-12 22:32:18 -0500

    Started working on cart page; doesn't work

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { ShopPageComponent } from "./pages/shop/shop-page.component";
 import { ProductComponent } from './pages/product/product.component';
+import { CartComponent } from './pages/cart/cart.component';
 
 export const routes: Routes = [
   { path: 'shop', component: ShopPageComponent },
   { path: 'product', component: ProductComponent },
-  { path: '', redirectTo: 'shop', pathMatch: 'full' }
+  { path: '', redirectTo: 'shop', pathMatch: 'full' },
+  { path: 'cart', component: CartComponent }
 ];
